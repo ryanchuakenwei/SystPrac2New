0660bb6165776e3f9ed071d612e3b2a6b361ebe7
ryanchuakenwei <a1898548@adelaide.edu.au>
2025-08-10 14:03:02 +0000

    rock

--- Computer.cpp
+++ Computer.cpp
@@ -19,16 +19,5 @@ std::string Computer::getName() const {
 }
 
 Move* Computer::makeMove() {
-    int r = std::rand() % 8;
-    switch (r) {
-        case 0: return new Rock();
-        case 1: return new Paper();
-        case 2: return new Scissors();
-        case 3: return new Robot();
-        case 4: return new Monkey();
-        case 5: return new Pirate();
-        case 6: return new Ninja();
-        case 7: return new Zombie();
-    }
-    return nullptr;
+    return new Rock();
 }
